66c59978a5ceb5620aabfee87da656ad69972f9f
Edwin <jessicawu414@gmail.com>
2022-02-11 13:45:45 -0500

    Fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import "../styles/tailwind.css"
+import "../styles/fonts.css"
 import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
